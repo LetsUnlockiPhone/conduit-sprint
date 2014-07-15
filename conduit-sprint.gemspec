0bd4e52bf43158a2d6757eeb9e343abbeb03b535
Pedro Martinez <pedro.martinez@bqsoft.com>
2014-07-15 16:26:01 -0400

    Adding rspec/its as a development dependency

--- conduit-sprint.gemspec
+++ conduit-sprint.gemspec
@@ -32,5 +32,5 @@ Gem::Specification.new do |s|
   #
   s.add_development_dependency 'shoulda-matchers'
   s.add_development_dependency 'rspec'
-
+  s.add_development_dependency 'rspec-its'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
+require 'rspec/its'
 require 'conduit/sprint'
 require 'conduit/sprint/driver'
+include Conduit::Driver::Sprint
 
 Dir[File.join(Dir.pwd, 'spec/support/**/*.rb')].each { |file| require file }
