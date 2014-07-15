ffc68aec2a252f25bc9ad371391cf748583b1d92
Doug Perez <doug.perez@bqsoft.com>
2014-07-15 10:11:03 -0400

    Add hotline api
    Adds hotline api

--- lib/conduit/sprint/actions/hotline.rb
+++ lib/conduit/sprint/actions/hotline.rb
@@ -2,6 +2,9 @@ require_relative 'base'
 
 module Conduit::Driver::Sprint
   class Hotline < Conduit::Driver::Sprint::Base
+    wsdl_service        'WholesaleSubscriptionModifyService/v1'
+    xsd                 'WholesaleSubscriptionModify/v1/ModifySubscriptionEnvelope.xsd'
+    operation           :suspend_subscription
     required_attributes :mdn
   end
 end
