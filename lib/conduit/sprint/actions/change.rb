22f1897ca00c063fb149bc43ebabb5b885c9fb3d
Doug <doug.perez@bqsoft.com>
2014-07-17 11:31:18 -0400

    change api

--- lib/conduit/sprint/actions/change.rb
+++ lib/conduit/sprint/actions/change.rb
@@ -2,6 +2,10 @@ require_relative 'base'
 
 module Conduit::Driver::Sprint
   class Change < Conduit::Driver::Sprint::Base
+    wsdl_service        'WholesaleSubscriptionModifyService/v1'
+    xsd                 'wholesaleChangeServicePlans/v3/wholesaleChangeServicePlansV3.xsd'
+    operation           :wholesale_change_service_plans_v3
     required_attributes :mdn
+    optional_attributes :plan_code, :services_to_add, :services_to_remove
   end
 end
