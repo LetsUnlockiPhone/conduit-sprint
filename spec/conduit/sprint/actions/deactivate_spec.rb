6e371013074f0bca52358af74d61b8863e736e04
Pedro Martinez <pedro.martinez@bqsoft.com>
2014-07-17 10:46:46 -0400

    Action: deactivate

---
+++ spec/conduit/sprint/actions/deactivate_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe Deactivate do
+  let(:deactivate) do
+    Deactivate.new(credentials.merge(mdn: '5555555555'))
+  end
+
+  let(:unsigned_soap) do
+    File.read('./spec/fixtures/requests/deactivate/unsigned_soap.xml')
+  end
+
+  let(:signed_soap) do
+    File.read('./spec/fixtures/requests/deactivate/signed_soap.xml')
+  end
+
+  let(:success) do
+    File.read('./spec/fixtures/responses/deactivate/success.xml')
+  end
+
+  describe 'soap_xml' do    
+    subject { deactivate.soap_xml }
+    it      { should eq unsigned_soap }
+  end
+
+  describe 'signed_soap_xml' do
+    subject { deactivate.signed_soap_xml }
+    it      { should eq signed_soap }
+  end
+
+  context 'a successful deactivate response is returned' do
+    before(:example) do
+      savon.expects(:expire_subscription).
+        with(signed_soap: signed_soap).returns(success)
+    end
+
+    subject                 { deactivate.perform }
+    it                      { should be_an_instance_of Deactivate::Parser }
+    its(:xml)               { should eq success }
+    its(:response_status)   { should eq 'success' }
+    its(:response_errors)   { should be_empty }
+    its(:serializable_hash) { should be_empty }
+  end
+end
