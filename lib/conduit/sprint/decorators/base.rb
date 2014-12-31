d717d3d08264f346870cf7ee89222fae37b829f4
Doug <doug.perez@hellolabs.com>
2014-12-30 23:39:22 -0500

    parse address

--- lib/conduit/sprint/decorators/base.rb
+++ lib/conduit/sprint/decorators/base.rb
@@ -1,8 +1,27 @@
+require 'street_address'
 require 'delegate'
 require 'forwardable'
 
 module Conduit::Sprint::Decorators
   class Base < SimpleDelegator
     extend Forwardable
+
+    def street_direction
+      parsed_address1.prefix if parsed_address1
+    end
+
+    def street_number
+      parsed_address1.number if parsed_address1
+    end
+
+    def street_name
+      (parsed_address1.street + ' ' + parsed_address1.street_type).strip if parsed_address1
+    end
+
+    private
+
+    def parsed_address1
+      @parsed_address1 ||= StreetAddress::US.parse(address1, informal: true)
+    end
   end
 end
