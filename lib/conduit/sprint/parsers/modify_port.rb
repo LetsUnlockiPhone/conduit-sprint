30294614d691c6c6ade9e6e0cdfc42c42c221e7c
Doug <doug.perez@hellolabs.com>
2015-04-23 09:18:50 -0400

    modify port will always be submitted

--- lib/conduit/sprint/parsers/modify_port.rb
+++ lib/conduit/sprint/parsers/modify_port.rb
@@ -2,5 +2,11 @@ require 'conduit/sprint/parsers/base'
 
 module Conduit::Driver::Sprint
   class ModifyPort::Parser < Parser::Base
+
+    private
+
+    def in_progress?
+      true
+    end
   end
 end
