218f3a84b3b168650ca8ac3be749a2f32f25c30d
Doug <doug.perez@hellolabs.com>
2015-05-05 14:07:56 -0400

    check for .blank?

--- lib/conduit/sprint/decorators/activate_port_decorator.rb
+++ lib/conduit/sprint/decorators/activate_port_decorator.rb
@@ -3,9 +3,9 @@ require_relative 'base'
 module Conduit::Sprint::Decorators
   class ActivatePortDecorator < Base
     def port_authorized_by
-      if authorized_by
+      if !authorized_by.blank?
         authorized_by
-      elsif first_name && last_name
+      elsif !first_name.blank? && !last_name.blank?
         (first_name + ' ' + last_name).strip
       end
     end
