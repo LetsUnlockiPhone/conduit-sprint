e5c9c613482c57f64dc5f205ac5bfa65e4a7caad
Doug <doug.perez@hellolabs.com>
2015-10-18 22:55:49 -0400

    enhance query messages
    - returns mdn
    - allow more query params

--- lib/conduit/sprint/actions/query_port_message.rb
+++ lib/conduit/sprint/actions/query_port_message.rb
@@ -5,6 +5,26 @@ module Conduit::Driver::Sprint
     wsdl_service        'WholesalePortMessageService/v1'
     xsd                 'queryPortMessage/v1/queryPortMessage.xsd'
     operation           :query_port_message
-    required_attributes :mdn
+    optional_attributes :mdn, :message_type_code, :reply_port_out_request,
+                        :message_status_code, :start_date, :end_date
   end
 end
+
+# message_type_code:
+# Provide to identify to query a specified message type.
+#  
+# EAI Message Type Codes
+# POR - PortOutRequest
+# DDT - DueDateTime
+# PIR - PortInResponse
+# PTS - PortStatus
+# PTA - PortAbandonment
+# PTN - PortNotifications
+# PTC - PortCancel
+# RPR - Reply PortOut Request
+
+# message_status_code:
+# Field is used to identify if an MVNO has responded to a PortOut request (either initial or supplemental)
+#  
+# Y indicates port out request has had a reply
+# N indicates port out request that has not had a reply.
