ffb6d5813e451066174a8ea6d814ef0965e75b5c
Doug <doug.perez@hellolabs.com>
2016-02-29 10:56:07 -0500

    set consumer_id from app_user_id

--- lib/conduit/sprint/actions/add_foreign_device.rb
+++ lib/conduit/sprint/actions/add_foreign_device.rb
@@ -5,8 +5,14 @@ module Conduit::Driver::Sprint
     wsdl_service        'DeviceManagementService/v1'
     xsd                 'addForeignDevice/v1/addForeignDevice.xsd'
     operation           :add_foreign_device
-    required_attributes :device_serial_number, :consumer_id
-    optional_attributes :part_number, :brand_code, :sku_type
+    required_attributes :device_serial_number
+    optional_attributes :part_number, :brand_code, :sku_type, :consumer_id
+
+    def initialize(options = {})
+      super
+      # Sprint wants a consumer_id in the api call, its the same as app_user_id
+      @options[:consumer_id] ||= @options[:application_user_id]
+    end
 
     def test_gateway
       "webservicesgatewaytest.sprint.com:444/#{test_environment}/services/wireless/device"
